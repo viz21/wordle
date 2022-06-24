a2ce9fe9d554c18e873fb9ab0f9c5feabe513f77
Vishwa <vishwa.g@affiniti.biz>
2022-06-24 08:42:43 +0530

    master branch is cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CustomInput from './component/CustomInput';
-
-const API_URL = 'https://www.algoexpert.io/api/fe/wordle-words';
 
 export const App = () => {
-  const [randomWord, setRandomWord] = useState('');
-  const [guessedWords, setGuessedWords] = useState(new Array(5).fill(''));
-
-  useEffect(() => {
-    const fetchWords = async () => {
-      const response = await fetch(API_URL);
-      const words: string[] = await response.json();
-      setRandomWord(words[Math.floor(Math.random() * words.length)]);
-    };
-
-    fetchWords();
-  }, []);
-
   return (
     <div className="App">
-      Random Word is: {randomWord}
-      {guessedWords.map((item) => {
-        return <CustomInput />;
-      })}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 };
